ffdf4dd97c87f30e430bb14059bc695bb0958ca9
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:14:41 +0400

    exercise 1

---
+++ Chapter 3- Data Structures/Exercises/exercise 1.py
@@ -0,0 +1,8 @@
+friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Rio", "Francis"]
+
+print (friends [0])
+print (friends [1])
+print (friends [2])
+print (friends [3])
+print (friends [4])
+print (friends [5])
