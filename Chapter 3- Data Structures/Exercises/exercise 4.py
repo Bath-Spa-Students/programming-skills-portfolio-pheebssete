5ca1ef85bc7df53d011244cb95ec999fe4ca8683
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:15:20 +0400

    exercise 4

---
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -0,0 +1,9 @@
+friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
+
+msg = "Hello!, you are invited to my late night dinner at my house, "
+
+print (msg + friends [0])
+print (msg + friends [1])
+print (msg + friends [2])
+print (msg + friends [3])
+print (msg + friends [4])
