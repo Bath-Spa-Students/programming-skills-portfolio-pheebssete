9e797e935c51300ad61ad2aba0a0ffdf376cb7ff
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:15:41 +0400

    exercise 6

---
+++ Chapter 3- Data Structures/Exercises/exercise 6.py
@@ -0,0 +1,28 @@
+# guess list 
+friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
+print ("Official Guest List: ")
+print (friends)
+print ()
+
+# Problem 
+friends_1 = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
+print(f"Sorry {friends_1.pop(3)} , I can't invite you to the late night dinner.")
+print(f"Sorry {friends_1.pop(4)} , I can't invite you to the late night dinner.")
+print(f"Sorry {friends_1.pop(2)} , I can't invite you to the late night dinner.")
+print ()
+
+# Guess who have been removed 
+removed = ["Jayde" , "Francis" , "Gwen"]
+msg = """ Due to some problem with table plan, 
+I can't invite you now to my late night dinner."""
+print (f'Sorry, {removed [0] + msg}')
+print (f'Sorry, {removed [1] + msg}')
+print (f'Soryy, {removed [2] + msg}')
+print ()
+
+# Guess who is still invited 
+inv = ["Leona" , "Aliyah"]  
+msg = " You are still invited to my late night dinner"
+print (f'Hello!, {inv [0] + msg}')
+print (f'Hello!, {inv [1] + msg}')
+
