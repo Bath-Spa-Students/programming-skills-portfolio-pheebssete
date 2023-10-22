5093f7229a9fadc28b25b2ccc4aad3efeb60a870
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:15:32 +0400

    exercise 5

---
+++ Chapter 3- Data Structures/Exercises/exercise 5.py
@@ -0,0 +1,18 @@
+friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
+
+# Daniella Can't Make It  
+print (friends [4] + " can't come to the late night dinner ")
+
+# I'll Invite Ken Instead 
+friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
+friends[4]= "Ken" 
+
+print ("But, " + friends [4] + " can come to the late night dinner!")
+
+# Friends That Is Still Invited 
+
+print ("Hello! " + friends [0] + ", you are invited to my late night dinner at my house.")
+print ("Hello! " + friends [1] + ", you are invited to my late night dinner at my house.")
+print ("Hello! " + friends [2] + ", you are invited to my late night dinner at my house.")
+print ("Hello! " + friends [3] + ", you are invited to my late night dinner at my house.")
+print ("Hello! " + friends [5] + ", you are invited to my late night dinner at my house.")
