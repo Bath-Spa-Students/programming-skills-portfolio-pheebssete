806578767d69dd075cf74f7b1d0e25e0f92105e0
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:14:29 +0400

    exercise 5

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 5.py
@@ -0,0 +1,15 @@
+# price of usb and her money 
+
+usb = 6
+cash = 50
+
+ans = cash // usb 
+
+print ("The girl can buy" , ans , " usb sticks")
+
+# amount of change 
+
+money = ans * usb
+change = cash - money
+
+print ("The girl has change worth " , change , " pounds")
