2cc6c2f82e038f67ba7f3515979fb2a15989ca92
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:15:12 +0400

    exercise 3

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -0,0 +1,8 @@
+transportation = ["Cafe Rider Honda " , "Honda Civic Type R" , "Porsche 911" , "Jeepney", "Jimny", "Jeep Wrangler"]
+
+print ("Today, I'm going to use my " + transportation [0])
+print ("I'm going to use my " + transportation [1])
+print ("I want to own a " + transportation [2])
+print ("I want to ride in " + transportation [3] + " for public transportation.")
+print (transportation [4] + " I want to own someday.")
+print (transportation [5] + " is one of my dream car for off road.")
