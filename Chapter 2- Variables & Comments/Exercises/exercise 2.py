88f9f449a9a6f5342282fb6aa14dda0eeafd7b8b
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:12:51 +0400

    exercise 2

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,9 @@
+# Quote from a famous person
+
+quote = '''"Everything you love, 
+You will eventually lose, 
+But in the end, 
+Love will return in defferent form"
+ - Franz Kafka'''
+
+print (quote)
