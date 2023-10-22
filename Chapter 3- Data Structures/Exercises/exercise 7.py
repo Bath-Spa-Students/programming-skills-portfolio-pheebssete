f940dbcb28709c9843ed282bde2e642c00233734
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:15:50 +0400

    exercise 7

---
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -0,0 +1,59 @@
+# dream places (original)
+places = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Dream Places: ")
+print (places)
+print ()
+
+# Alphabetical Order
+places_1 = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Alphabetical Order: ")
+s = sorted(places_1)
+print (s)
+print ()
+
+# Original Order 
+places = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Dream Places: ")
+print (places)
+print ()
+
+# Reverse Alphabetical 
+places_2 = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Reverse Alphabetical: ")
+r = sorted(places_2, reverse=True)
+print(r)
+print()
+
+# Original Order 
+places = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Dream Places: ")
+print (places)
+print ()
+
+# Reversed
+places_3 = ["Paris" , "San Francisco" , "Japan" , "South Korea" , "New York"]
+print ("Reversed: ")
+places_3.reverse()
+print (places_3)
+print ()
+
+# Reversed back 
+places_4 = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Reversed Back: ")
+places_4.reverse()
+print (places_4)
+print ()
+
+# Alphabetical 
+places_5 = ["Paris" , "San Francisco" , "Japan" , "South Korea" , "New York"]
+print ("Alphabetical: ")
+places_5.sort()
+print (places_5)
+print ()
+
+# Reverse Alphabetical 
+places_6 = ["New York City" , "South Korea" , "Japan" , "San Francisco" , "Paris"]
+print ("Reverse Alphabetical: ")
+places_6.sort()
+places_6.reverse()
+print (places_6)
