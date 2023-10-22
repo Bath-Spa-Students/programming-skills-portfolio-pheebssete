4540c63606f75a4ef4a502b707c9507a863800f9
pheebssete <rosetephoebe15@gmail.com>
2023-10-22 23:15:01 +0400

    exercise 2

---
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -0,0 +1,10 @@
+friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Ken"]
+
+message = "Missing you little extra today, "
+
+print (f"""{message + friends [0]}
+{message + friends [1]}
+{message + friends [2]}
+{message + friends [3]}
+{message + friends [4]}
+{message + friends [5]}""")
