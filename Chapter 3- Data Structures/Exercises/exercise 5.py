0652cbfe45598ce0c109229feef3f2d0ca515827
pheebssete <rosetephoebe15@gmail.com>
2023-10-29 23:23:55 +0400

    exercise 5

--- Chapter 3- Data Structures/Exercises/exercise 5.py
+++ Chapter 3- Data Structures/Exercises/exercise 5.py
@@ -1,18 +1,26 @@
-friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
+# Friends who are invited 
+friends = ["Leona", "Aliyah", "Gwen", "Jayde"]
+msg = "Hello!, you are invited to my late night dinner at my house, "
 
-# Daniella Can't Make It  
-print (friends [4] + " can't come to the late night dinner ")
+print (msg + friends [0])
+print (msg + friends [1])
+print (msg + friends [2])
+print (msg + friends [3])
+print ()
+
+# Jayde Can't Make It  
+print (friends [3] + " can't come to the late night dinner ")
+print()
 
 # I'll Invite Ken Instead 
-friends = ["Leona", "Aliyah", "Gwen", "Jayde", "Daniella", "Francis"]
-friends[4]= "Ken" 
+friends = ["Leona", "Aliyah", "Gwen", "Jayde"]
+friends[3]= "Ken" 
 
-print ("But, " + friends [4] + " can come to the late night dinner!")
+print ("But, " + friends [3] + " can come to the late night dinner!")
+print ()
 
-# Friends That Is Still Invited 
-
-print ("Hello! " + friends [0] + ", you are invited to my late night dinner at my house.")
-print ("Hello! " + friends [1] + ", you are invited to my late night dinner at my house.")
-print ("Hello! " + friends [2] + ", you are invited to my late night dinner at my house.")
-print ("Hello! " + friends [3] + ", you are invited to my late night dinner at my house.")
-print ("Hello! " + friends [5] + ", you are invited to my late night dinner at my house.")
+# Friends That Is Invited 
+print (msg + friends [0])
+print (msg + friends [1])
+print (msg + friends [2])
+print (msg + friends [3])
