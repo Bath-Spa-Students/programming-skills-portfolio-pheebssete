550d157e7c77e35c52fb415ec730763dfc733227
pheebssete <rosetephoebe15@gmail.com>
2023-11-04 00:34:52 +0400

    exercise 1

---
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -0,0 +1,11 @@
+# Storing information 
+
+person = { 'first_name ' : 'Jake' , 
+          'last_name ' : 'Lagazo' , 
+          'age ' : '20' , 
+          'city ' : 'Los Angles'}
+
+print (person['first_name '])
+print (person['last_name '])
+print (person['age '])
+print (person['city '])
