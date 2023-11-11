bdd0a50047310274e716992a3dc4017968b7bffa
pheebssete <rosetephoebe15@gmail.com>
2023-11-11 17:10:31 +0400

    exercise 2

---
+++ Chapter 5- Dictionaries/Exercises/exercise 2.py
@@ -0,0 +1,22 @@
+# Storing five programming words
+
+glossary = {'integers': 'Are whole number datas.',
+    'floats': 'Are numbers with decimal point.',
+    'string concatenation': 'Joining two or more strings by using (+) or (,).',
+    'list': 'Is a collection of iteams in a particular order.',
+    'print': 'Is used to send message or output to the screen.',}
+
+word = 'integers'
+print(f"\n{word.capitalize()}: {glossary[word]}")
+
+word = 'floats'
+print(f"\n{word.capitalize()}: {glossary[word]}")
+
+word = 'string concatenation'
+print(f"\n{word.capitalize()}: {glossary[word]}")
+
+word = 'list'
+print(f"\n{word.capitalize()}: {glossary[word]}")
+
+word = 'print'
+print(f"\n{word.capitalize()}: {glossary[word]}")
