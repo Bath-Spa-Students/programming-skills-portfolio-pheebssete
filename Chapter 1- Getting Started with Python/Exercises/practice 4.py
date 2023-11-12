a7cd4bb206ffdd7247781feb805dd792dfa4707e
pheebssete <rosetephoebe15@gmail.com>
2023-11-12 14:31:31 +0400

    practice 4

---
+++ Chapter 1- Getting Started with Python/Exercises/practice 4.py
@@ -0,0 +1,10 @@
+# find area of a triangle
+
+base = float(input("Enter the base of a triangle: "))
+height = float(input("Enter the height of a triangle: "))
+
+# compute 
+area = (base * height) / 2
+
+# display 
+print ("The area of a triangle using", base, "and", height)
