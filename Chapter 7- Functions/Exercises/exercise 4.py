c3aa24b2edf0813395337caefc492b4e8b861e37
pheebssete <rosetephoebe15@gmail.com>
2023-11-14 10:03:30 +0400

    exercise 4

---
+++ Chapter 7- Functions/Exercises/exercise 4.py
@@ -0,0 +1,8 @@
+def make_shirt(shirt_size='large', message_in_the_shirt='I love Python!'):
+    print()
+    print("Your size " + (shirt_size) + " in t-shirt.")
+    print("T-shirt's print is " + (message_in_the_shirt))
+make_shirt()
+make_shirt(shirt_size='medium')
+print()
+make_shirt('small', 'Live, Laugh, Love')
