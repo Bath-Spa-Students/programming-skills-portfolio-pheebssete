3d71c608d1ffdc675edbf9f92c74ab6161d05359
pheebssete <rosetephoebe15@gmail.com>
2023-11-14 10:03:22 +0400

    exercise 3

---
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -0,0 +1,7 @@
+def make_shirt(shirt_size, text_of_a_message):
+    print("Your size " + (shirt_size) + " in t-shirt.")
+    print("The print on your shirt is, "+ (text_of_a_message) +"!")
+    
+make_shirt('small', 'Live, Laugh, Love, F1')
+print()
+make_shirt(text_of_a_message="Live, Laugh, Love, Gym", shirt_size='extra large shirt')
