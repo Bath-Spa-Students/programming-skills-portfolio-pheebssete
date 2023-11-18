6d16404fbe0a2179958621a3d65f7e404388fef7
pheebssete <rosetephoebe15@gmail.com>
2023-11-18 14:29:15 +0400

    practice 3

---
+++ Chapter 2- Variables & Comments/Exercises/practice 3.py
@@ -0,0 +1,13 @@
+# 5 input from user 
+name = (input("Please, Enter your name: "))
+age = int(input("Please, Enter your age: "))
+bday = float(input("Please, Enter your birthday (mm.dd): "))
+place = (input("Please, Enter your adress: "))
+fav = int(input("Please, Enter your favorite number: "))
+
+
+print(f"String:{name}")
+print(f"String:{place}")
+print(f"Integer:{age}")
+print(f"Interger:{fav}")
+print(f"Float:{bday}")
